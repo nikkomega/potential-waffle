a75c1dfe58cf8451476fd1b8c85a39b0686723b0
nikkomega <jeff.omega@gmail.com>
2022-08-08 15:56:26 -0500

    partial search better than no search

--- src/azspcs/core.clj
+++ src/azspcs/core.clj
@@ -2,46 +2,6 @@
   (:require [clojure.set :as set]
             [clojure.string :as str]))
 
-(defn neighbors [[x y]]
-  (for [dx [-1 0 1] dy [-1 0 1] :when (not= 0 dx dy)] [(+ x dx) (+ y dy)]))
-
-(defn openings [grid]
-  (let [coords (set (keys grid))]
-    (set/difference
-      (reduce set/union #{} (map (comp set neighbors) coords))
-      coords)))
-
-(defn neighbor-sum [grid p]
-  (reduce + (map #(get grid % 0) (neighbors p))))
-
-(defn choices [grid n]
-  (filter
-    #(= n (neighbor-sum grid %))
-    (openings grid)))
-
-(defn maximize [grid n]
-  (let [choices (choices grid n)
-        new-grids (map #(maximize (conj grid [% n]) (inc n)) choices)]
-    (if (empty? choices)
-      grid
-      (apply max-key count new-grids))))
-
-(defn randstart [n]
-  (let [size 5 #_n
-        points (for [x (range (- size) size)
-                     y (range (- size) size)]
-                 [x y])]
-    (into {} (map #(vector % 1) (take n (shuffle points))))))
-
-(defn search [n t]
-  (let [solution
-        (time
-          (apply max-key count
-                 (take t (repeatedly #(maximize (randstart n) 2)))))]
-    (println solution)
-    (println (submit solution))
-    (- (inc (count solution)) n)))
-
 (defn format-line [grid y x1 x2]
   (as->
     (for [x (range x1 x2)] (get grid [x y] 0)) line
@@ -61,3 +21,54 @@
     (str/join ", "
               (for [y (range y1 y2)]
                 (format-line grid y x1 x2)))))
+
+(defn neighbors [[x y]]
+  (for [dx [-1 0 1] dy [-1 0 1] :when (not= 0 dx dy)] [(+ x dx) (+ y dy)]))
+
+(defn openings [grid]
+  (let [coords (set (keys grid))]
+    (set/difference
+      (reduce set/union #{} (map (comp set neighbors) coords))
+      coords)))
+
+(defn neighbor-sum [grid p]
+  (reduce + (map #(get grid % 0) (neighbors p))))
+
+(defn choices [grid n]
+  (filter
+    #(= n (neighbor-sum grid %))
+    (openings grid)))
+
+(defn maximize [grid n]
+  (let [best (atom 0)
+        maximize' (fn maximize' [grid n]
+                    (let [choices (choices grid n)
+                          new-grids (map #(maximize' (conj grid [% n]) (inc n)) choices)]
+                      (if (empty? choices)
+                        (if (> n @best)
+                          (do
+                            (println grid)
+                            (println (submit grid))
+                            (println n)
+                            (swap! best (constantly (count grid)))
+                            grid)
+                          grid)
+                        (apply max-key count new-grids))))]
+    (maximize' grid n)))
+
+(defn randstart [n]
+  (let [size 6
+        points (for [x (range (- size) size)
+                     y (range (- size) size)]
+                 [x y])]
+    (into {} (map #(vector % 1) (take n (shuffle points))))))
+
+(defn search [n t]
+  (let [solution
+        (time
+          (apply max-key count
+                 (take t (repeatedly #(maximize (randstart n) 2)))))]
+    (println solution)
+    (println (submit solution))
+    (print (- (inc (count solution)) n))
+    nil))
